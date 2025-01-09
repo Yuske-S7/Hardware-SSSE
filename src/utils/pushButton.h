f56fd922eea4cfd8cee14849596b5025a3db52f1
Nicolas Bruguet <nicolas.bruguet@epita.fr>
2025-01-09 17:08:09 +0100

    feat(pushButton): Ajout des librairies

--- src/utils/pushButton.h
+++ src/utils/pushButton.h
@@ -1,6 +1,9 @@
 #ifndef PUSHBUTTON_H
 #define PUSHBUTTON_H
 
+#include <driver/gpio.h>
+#include <stdio.h>
+
 const int SECONDS_PUSHBUTTON_GPIO_PIN = 35;
 const int MINUTES_PUSHBUTTON_GPIO_PIN = 36;
 const int HOURS_PUSHBUTTON_GPIO_PIN = 39;
