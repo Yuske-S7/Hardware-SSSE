87170df490805115b89433457948e392be2cb9e3
Nicolas <nicolas_marc@outlook.com>
2025-01-09 17:07:33 +0100

    (feat): ajout BUZZER test - v2

--- src/utils/buzzer.c
+++ src/utils/buzzer.c
@@ -1,11 +1,34 @@
 #include "buzzer.h"
 
-int trigger_buzzer(int seconds) {
-    if (gpio_set_level(BUZZER_GPIO_PIN, 1) == ESP_ERR_INVALID_ARG)
-        return 0;
-    vTaskDelay(seconds * 1000 / portTICK_PERIOD_MS);
-    if (gpio_set_level(BUZZER_GPIO_PIN, 0) == ESP_ERR_INVALID_ARG)
-        return 0;
+void buzzer_init() {
+    ledc_timer_config_t ledc_timer = {
+        .speed_mode = LEDC_LOW_SPEED_MODE,
+        .timer_num = LEDC_TIMER_0,
+        .duty_resolution = LEDC_TIMER_13_BIT,
+        .freq_hz = BUZZER_PWM_FREQ,
+        .clk_cfg = LEDC_AUTO_CLK
+    };
+    ledc_timer_config(&ledc_timer);
 
-    return 1;
+    ledc_channel_config_t ledc_channel = {
+        .gpio_num = BUZZER_GPIO_PIN,
+        .speed_mode = LEDC_LOW_SPEED_MODE,
+        .channel = LEDC_CHANNEL_0,
+        .timer_sel = LEDC_TIMER_0,
+        .duty = 0,
+        .hpoint = 0
+    };
+    ledc_channel_config(&ledc_channel);
+}
+
+void buzzer_start(int duration_ms) {
+    ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 4096); // Duty cycle 50%
+    ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+    vTaskDelay(duration_ms / portTICK_PERIOD_MS);
+    buzzer_stop(); // Arrêter après la durée
+}
+
+void buzzer_stop() {
+    ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 0); // Duty cycle 0%
+    ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
 }
--- src/utils/buzzer.h
+++ src/utils/buzzer.h
@@ -5,9 +5,13 @@
 #include <driver/adc.h> 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include <driver/ledc.h>
 
 #define BUZZER_GPIO_PIN 2 // 24
+#define BUZZER_PWM_FREQ 2000
 
-int trigger_buzzer(int seconds);
+void buzzer_start(int duration_ms);
+void buzzer_stop(void);
+void buzzer_init();
 
 #endif //BUZZER_H
