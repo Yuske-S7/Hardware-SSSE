b21a8469328f88287975439207b280b4915562a6
Nicolas Bruguet <nicolas.bruguet@epita.fr>
2025-01-09 17:13:35 +0100

    fix(trigger_timer): modi gpio_set_level to gpio_get_level

--- src/utils/pushButton.c
+++ src/utils/pushButton.c
@@ -15,17 +15,17 @@ void trigger_timer(int seconds)
 
     while (1)
     {
-        if (gpio_set_level(SECONDS_PUSHBUTTON_GPIO_PIN) == 0)
+        if (gpio_get_level(SECONDS_PUSHBUTTON_GPIO_PIN) == 0)
         {
             printf("Seconds push button: (GPIO %d) pressed\n", SECONDS_PUSHBUTTON_GPIO_PIN);
             vTaskDelay(200 / portTICK_PERIOD_MS);
         }
-        if (gpio_set_level(MINUTES_PUSHBUTTON_GPIO_PIN) == 0)
+        if (gpio_get_level(MINUTES_PUSHBUTTON_GPIO_PIN) == 0)
         {
             printf("Minutes push button: (GPIO %d) pressed\n", MINUTES_PUSHBUTTON_GPIO_PIN);
             vTaskDelay(200 / portTICK_PERIOD_MS);
         }
-        if (gpio_set_level(HOURS_PUSHBUTTON_GPIO_PIN) == 0)
+        if (gpio_get_level(HOURS_PUSHBUTTON_GPIO_PIN) == 0)
         {
             printf("Hours push button: (GPIO %d) pressed\n", HOURS_PUSHBUTTON_GPIO_PIN);
             vTaskDelay(200 / portTICK_PERIOD_MS);
